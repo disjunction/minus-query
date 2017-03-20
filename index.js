e5e92fe951bd51745030034b901f955c092a98ff
Konstantin <or@pluseq.com>
2017-03-20 21:38:53 +0200

    extend

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-function decode (o) {
+function decode (o, opts) {
+  opts = opts || {}
+  let decoded = decodeMongo(o)
+  if (opts.dialect === 'sequelize') {
+    decoded = transformSequelize(decoded)
+  }
+  return decoded
+}
+
+function decodeMongo (o) {
   const result = {}
   for (let key in o) {
     const matches = String(key).match(/^(or-)?(not-)?(\w+)-(.*)$/)
     if (matches) {
-      result[matches[4]] = {['$' + matches[3]]: o[key]}
+      let value = o[key]
+      if (matches[3] === 'in') {
+        value = String(value).split(',')
+      }
+      let compare = {['$' + matches[3]]: value}
+      if (matches[2]) {
+        switch(matches[3]) {
+          case 'eq':
+            compare = {$ne: value}
+            break
+          case 'in':
+            compare = {$nin: value}
+            break
+          default:
+            compare = {$not: compare}
+        }
+      }
+      result[matches[4]] = compare
     }
   }
   return result
+}
+
+function transformSequelize (o) {
+  return o
 }
 
 module.exports = {
--- spec/test.spec.js
+++ spec/test.spec.js
@@ -2,10 +2,18 @@
 const minusQuery = require('./../index')
 
 describe('decode', () => {
-  it('decodes', () => {
+  it('decodes trivial case', () => {
     const input = {'eq-abc': 7, 'lte-my-field': 'ok'}
     const result = minusQuery.decode(input)
     expect(result.abc).toEqual({$eq: 7});
     expect(result['my-field']).toEqual({$lte: 'ok'});
+  })
+
+  it('decodes with not', () => {
+    const input = {'not-eq-abc': 7, 'not-lte-my-field': 'ok', 'not-in-list': '5,3,9'}
+    const result = minusQuery.decode(input)
+    expect(result.abc).toEqual({$ne: 7});
+    expect(result['my-field']).toEqual({$not: {$lte: 'ok'}});
+    expect(result.list).toEqual({$nin: ['5', '3', '9']});
   })
 })
